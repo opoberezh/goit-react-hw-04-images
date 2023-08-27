b9bb81ed900adbad8f8b261e629b8aa0cbc78411
opoberezh <opoberezh.2015@gmail.com>
2023-08-27 16:42:18 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ import { useEffect } from 'react';
 
 
 
-export const App =() => {
+export const App = () => {
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
@@ -29,69 +29,58 @@ export const App =() => {
     setIsLastPage(false);
   };
 
-  useEffect(()=>{ 
-    if(query === '')  return
-    async function fetchImages  () {
-  const separatedQuery = query.split('/')[1];
- 
- 
-  try {
-    setLoading (true); 
-   
-    const {hits, totalHits} = await getImages({ query: separatedQuery , page});
-    setImages(prevState =>[...prevState, ...hits]);
-    setIsLastPage(prevState =>prevState + hits >= totalHits); 
-    setError(null); 
+  useEffect(() => {
+    if (query === '') return;
 
-   
-   
-  } catch (error) {
-    console.log(error);
-    toast.error('Something went wrong!', {
-      icon: '🤯',
-    });
-  } finally {
-    setLoading(false);
-  }
-   
-    
-};
-  fetchImages();
-  }, [query, page])
+    async function fetchImages() {
+      const separatedQuery = query.split('/')[1];
 
- 
+      try {
+        setLoading(true);
+        const { hits, totalHits } = await getImages({ query: separatedQuery, page });
+        setImages(prevState => [...prevState, ...hits]);
+        setIsLastPage(page * 12 >= totalHits);
+        setError(null);
+      } catch (error) {
+        console.log(error);
+        toast.error('Something went wrong!', {
+          icon: '🤯',
+        });
+      } finally {
+        setLoading(false);
+      }
+    }
 
- const handleLoadMore = () => {
-  if (query.trim() === '') {
-     toast("🦄 Oops! Search query is empty!");
-     return;
-    } 
+    fetchImages();
+  }, [query, page]);
+
+  const handleLoadMore = () => {
+    if (query.trim() === '') {
+      toast("🦄 Oops! Search query is empty!");
+      return;
+    }
     setPage(prevState => prevState + 1);
-    };
-    
- 
+  };
 
- return (
-      <>
-        <div>
-          <SearchBar onSubmit={changeQuery} />
-          {loading && <Loader />}
-          {error && !loading && (
-            toast.error("Something went wrong!", {
-              icon: "😲"
-            })
-          )}
-          <ImageGallery images={images} />
-          {images.length > 0 && !loading && !isLastPage && (
-            <LoadMoreButton onClick={handleLoadMore} />
-          )}
-          
-          <Modal />
-        </div>
-        <ToastContainer position="top-right" autoClose={2000} />
-      </>
-    );
-  
+  return (
+    <>
+      <div>
+        <SearchBar onSubmit={changeQuery} />
+        {loading && <Loader />}
+        {error && !loading && (
+          toast.error("Something went wrong!", {
+            icon: "😲"
+          })
+        )}
+        <ImageGallery images={images} />
+        {images.length > 0 && !loading && !isLastPage && (
+          <LoadMoreButton onClick={handleLoadMore} />
+        )}
+        <Modal />
+      </div>
+      <ToastContainer position="top-right" autoClose={2000} />
+    </>
+  );
 }
   
   
